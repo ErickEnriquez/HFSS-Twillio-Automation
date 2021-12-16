1aa7e61e30c0d40d7b56831211b8c17fc42f8fcb
Erick Enriquez <erick.enriquez10.95@gmail.com>
2021-12-16 13:27:04 -0700

    getting ready to send, include excelJS

--- text.js
+++ text.js
@@ -1,17 +1,43 @@
 const config = require('./config')
+const ExcelJS = require('exceljs')
 
-
-async function sendMessage ({ accountSid, authToken, from }, to) {
+async function sendMessage ({ accountSid, authToken, from }, { message, mediaList }) {
 	try {
+		const names = await readCSV('/Users/erick/Downloads/names.csv')
+
 		const client = require('twilio')(accountSid, authToken)
-		await client.messages
-			.create({
-				body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
-				from: from,
-				mediaUrl: ['https://c1.staticflickr.com/3/2899/14341091933_1e92e62d12_b.jpg'],
-				to: to
-			})
+		names.forEach(name => {
+			const msg = client.messages
+				.create({
+					body: message,
+					from: from,
+					mediaUrl: mediaList,
+					to: name[4]
+				}).then(mes => console.log(mes.status)).catch(err => console.log(err, names[4]))
+		})
 	} catch (error) { console.error(error) }
 }
 
-sendMessage({ accountSid: config.accountSid, authToken: config.authToken, from: config.sendingNumber }, '4804386672')
+const readCSV = async (filePath) => {
+	const workbook = new ExcelJS.Workbook()
+	const sheet = await workbook.csv.readFile(filePath)
+	let data = []
+	sheet.eachRow((row, num) => {
+		if (num !== 1) {
+			data = [...data, row.values]
+		}
+	})
+	return data
+}
+
+sendMessage(
+	{ accountSid: config.accountSid, authToken: config.authToken, from: config.sendingNumber },
+
+	{
+		message: 'From HFSS' + '\n\n' + 'please send this out to your family' + '\n\n' + 'reply STOP to unsubscribe',
+		mediaList: [
+			'https://hfss-website.s3.us-west-2.amazonaws.com/now_hiring_2.png',
+			'https://hfss-website.s3.us-west-2.amazonaws.com/now_hiring_1.png'
+		]
+	}
+)
