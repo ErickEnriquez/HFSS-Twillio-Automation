ffb153361dbdd12ea8dfae12bca6a7244c636f81
Erick Enriquez <erick.enriquez10.95@gmail.com>
2021-12-17 12:58:53 -0700

    [update] added function to format the phone numbers since some might be in weird formats and may get rejected from twillio otherwise

--- text.js
+++ text.js
@@ -3,21 +3,23 @@ const ExcelJS = require('exceljs')
 
 async function sendMessage ({ accountSid, authToken, from }, { message, mediaList }) {
 	try {
-		const names = await readCSV('/Users/erick/Downloads/names.csv')
+		const list = await readCSV('/Users/erick/Downloads/names.csv')
 
 		const client = require('twilio')(accountSid, authToken)
-		names.forEach(name => {
+		list.forEach(item => {
+			const phoneNumber = String(formatPhoneNumber(item[3]))
 			const msg = client.messages
 				.create({
 					body: message,
 					from: from,
 					mediaUrl: mediaList,
-					to: name[4]
-				}).then(mes => console.log(mes.status)).catch(err => console.log(err, names[4]))
+					to: phoneNumber
+				}).then(mes => console.log(mes.status)).catch(err => console.log(err, item[3]))
 		})
 	} catch (error) { console.error(error) }
 }
 
+//read the data from the csv file using exceljs and return a list with names as well as phone numbers
 const readCSV = async (filePath) => {
 	const workbook = new ExcelJS.Workbook()
 	const sheet = await workbook.csv.readFile(filePath)
@@ -28,6 +30,21 @@ const readCSV = async (filePath) => {
 		}
 	})
 	return data
+}
+
+//take the phone numbers from csv file and format them all so twillio can send them all out correctly
+const formatPhoneNumber = (str) => {
+	//Filter only numbers from the input
+	let cleaned = ('' + str).replace(/\D/g, '')
+
+	//Check if the input is of correct length
+	let match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/)
+
+	if (match) {
+		return '(' + match[1] + ') ' + match[2] + '-' + match[3]
+	}
+
+	return str
 }
 
 sendMessage(
